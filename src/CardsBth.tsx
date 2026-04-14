983a41152ecd7464cc952dfc0f8448d1f61a1c34
Anton <antonkarko33@gmail.com>
2026-04-14 22:39:01 +0300

    fix: add file CardBth

--- src/CardsBth.tsx
+++ src/CardsBth.tsx
@@ -1,12 +1,12 @@
-import styled, {css} from "styled-components";
+import styled from "styled-components";
 
-// import {CardBth} from "./CardBth";
+import {CardBth} from "./CardBth";
 
 export function CardsBth() {
     return (
         <StyledCardsBth>
-            <CardBth typeBth={"primary"} title={"See more"}>See more</CardBth>
-            <CardBth typeBth={"outline"} title={"Save"}>Save</CardBth>
+            <CardBth typeBth={"primary"} title={"See more"}></CardBth>
+            <CardBth typeBth={"outline"} title={"Save"}></CardBth>
         </StyledCardsBth>
     )
 }
@@ -17,44 +17,4 @@ const StyledCardsBth = styled.div`
     display: flex;
     justify-content: flex-start;
     gap: 10%;
-
-`
-
-type StyleCardBrhPropsType = {
-    title?: string;
-    color?: string;
-    typeBth?: "primary" | "outline";
-}
-
-const CardBth = styled.button<StyleCardBrhPropsType>`
-    border: none;
-    background-color: ${props => props.color || "#4E71FE"};
-    width: 86px;
-    height: 30px;
-    //padding: 1% 6%;
-    border-radius: 5px;
-    color: snow;
-    font-weight: 700;
-    font-size: 10px;
-    line-height: 20px;
-    letter-spacing: 0;
-
-    &:hover {
-        background-color: rgb(66 68 87);
-        border: none;
-        color: rgb(183 187 221);
-    }
-
-    ${props => props.typeBth === "outline" && css<StyleCardBrhPropsType>`
-        border: 2px solid ${props.color || "#4E71FE"};
-        background-color: #ffffff;
-        color: #4E71FE;
-    `
-    }
-    ${props => props.typeBth === "primary" && css<StyleCardBrhPropsType>`
-        border: 2px solid ${props.color || "#ffffff"};
-        background-color: #4E71FE;
-        color: #ffffff;
-    `
-    }
 `
